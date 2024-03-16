21ecee53a85d78ffeacaa6672a0f6a20b8594167
mouradghaly <101068633+mouradghaly@users.noreply.github.com>
2024-03-16 10:00:08 +0200

    Update settings.py

--- iftarapp/iftarapp/settings.py
+++ iftarapp/iftarapp/settings.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.auth.middleware.RemoteUserMiddleware',
